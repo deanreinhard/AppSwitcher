e0f38df29f09c868fca7475bfe13dc7a5b742eef
xanxys <xanxys@gmail.com>
2013-06-29 15:58:33 +0900

    issue #4: port most code to Direct2D. Only remaining GDI code is RiftWindow::CaptureScreen, which is very complicated to write in newer APIs.

--- AppSwitcher/RiftWindow.h
+++ AppSwitcher/RiftWindow.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <Windows.h>
+#include <d2d1.h>
+#include <wincodec.h>
 #include <vector>
 #include <OVR.h>
 #include <vcclr.h>
@@ -23,7 +25,6 @@ namespace AppSwitcher {
 			pin_ptr<const wchar_t> wch = PtrToStringChars(device);
 			EnumDisplaySettingsW(wch, ENUM_CURRENT_SETTINGS, &devmode);
 
-
 			WNDCLASSW wc = {0};
 			wc.hInstance = GetModuleHandle(NULL);
 			wc.lpfnWndProc = reinterpret_cast<WNDPROC>(WndProc);
@@ -42,11 +43,27 @@ namespace AppSwitcher {
 				GetModuleHandle(NULL),
 				this);	//pass the window a pointer to this Scope object
 			ShowWindow(m_hwnd, SW_SHOW);
+
+			// prepare Direct2D
+			direct2DFactory = nullptr;
+			D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &direct2DFactory);
+			RECT rect;
+			GetClientRect(m_hwnd, &rect);
+
+			renderTarget = nullptr;
+			direct2DFactory->CreateHwndRenderTarget(D2D1::RenderTargetProperties(),
+				D2D1::HwndRenderTargetProperties(m_hwnd, D2D1::SizeU(rect.right-rect.left, rect.bottom-rect.top)),
+				&renderTarget);
 		}
 
 	protected:
 		~RiftWindow()
 		{
+			if(renderTarget)
+				renderTarget->Release();
+
+			if(direct2DFactory)
+				direct2DFactory->Release();
 		}
 
 		static LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
@@ -75,35 +92,67 @@ namespace AppSwitcher {
 			}
 		}
 
+		HBITMAP CaptureScreen(int x0, int y0){
+			HDC dc_screen = GetDC(0);
+			HDC dc_bitmap = CreateCompatibleDC(dc_screen);
+
+			HBITMAP bitmap = CreateCompatibleBitmap(dc_screen, 1280, 800);
+			SelectObject(dc_bitmap, bitmap);
+
+			BitBlt(dc_bitmap, 0, 0, 1280, 800, dc_screen, x0, y0, SRCCOPY);
+
+			ReleaseDC(0, dc_screen);
+			DeleteDC(dc_bitmap);
+
+			return bitmap;
+		}
+
 		void RenderFrame(){
-			int sz = 100;
-			int d = 70;
+			const int sz = 100;
+			const int d = 70;
 
-			PAINTSTRUCT ps;
-			HDC dc_targ = BeginPaint(m_hwnd, &ps);
-
-			
+			// capture portion of screen
+			ID2D1Bitmap* bitmap_d2d = nullptr;
 			if(window_app){
 				POINT pt;
 				pt.x = 0;
 				pt.y = 0;
 				ClientToScreen(window_app, &pt);
 
-				HDC dc_all = GetDC(0);
-				BitBlt(dc_targ, 0, 0, 1280, 800, dc_all, pt.x, pt.y, SRCCOPY);
-				ReleaseDC(0, dc_all);
+				HBITMAP bitmap = CaptureScreen(pt.x, pt.y);
+
+				// convert to D2D bitmap
+				IWICImagingFactory* iFactory = nullptr;
+				CoCreateInstance(CLSID_WICImagingFactory, NULL, CLSCTX_INPROC_SERVER, IID_IWICImagingFactory, reinterpret_cast<LPVOID*>(&iFactory));
+
+				IWICBitmap* bitmap_iwic = nullptr;
+				iFactory->CreateBitmapFromHBITMAP(bitmap, NULL, WICBitmapAlphaChannelOption::WICBitmapIgnoreAlpha, &bitmap_iwic);
+
+				renderTarget->CreateBitmapFromWicBitmap(bitmap_iwic, &bitmap_d2d);
+
+				bitmap_iwic->Release();
+				iFactory->Release();
+				DeleteObject(bitmap);
 			}
 
+			// render
+			ID2D1SolidColorBrush* brush = nullptr;
+			renderTarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::Beige), &brush);
 
-			RoundRect(dc_targ, 320+d-sz, 250, 320+d+sz, 400, 3, 3);
-			RoundRect(dc_targ, 640+320-d-sz, 250, 640+320-d+sz, 400, 3, 3);
-			
-			TextOutW(dc_targ, 320+d, 300, L"Yunalus", 7);
-			TextOutW(dc_targ, 640+320-d, 300, L"Yunalus", 7);
+			renderTarget->BeginDraw();
 
-			TextOutW(dc_targ, 320+d, 300+30, L"Mikulus", 7);
-			TextOutW(dc_targ, 640+320-d, 300+30, L"Mikulus", 7);
-			EndPaint(m_hwnd, &ps);
+			if(bitmap_d2d){
+				renderTarget->DrawBitmap(bitmap_d2d);
+				bitmap_d2d->Release();
+			}
+
+			renderTarget->DrawRectangle(D2D1::RectF(320+d-sz, 250, 320+d+sz, 400), brush);
+			renderTarget->DrawRectangle(D2D1::RectF(640+320-d-sz, 250, 640+320-d+sz, 400), brush);
+					
+			renderTarget->EndDraw();
+
+			if(brush)
+				brush->Release();
 		}
 
 		LRESULT HandleMessage(UINT message, WPARAM wParam, LPARAM lParam)
@@ -123,6 +172,9 @@ namespace AppSwitcher {
 	public:
 		HWND m_hwnd;
 		HWND window_app;
+	private:
+		ID2D1Factory* direct2DFactory;
+		ID2D1HwndRenderTarget* renderTarget;
 	};
 }
 
