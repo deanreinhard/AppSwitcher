517c123e9260e4c18262adff6e5c91999f7423e8
xanxys <xanxys@gmail.com>
2013-07-04 18:15:41 +0900

    include build date in error report

--- AppSwitcher/Entry.cpp
+++ AppSwitcher/Entry.cpp
@@ -4,6 +4,32 @@ using namespace System;
 using namespace System::IO;
 using namespace System::Windows::Forms;
 
+/// Get timestamp from the binary itself.
+/// http://stackoverflow.com/questions/1600962/displaying-the-build-date
+DateTime RetrieveLinkerTimestamp(){
+	String^ filePath = System::Reflection::Assembly::GetCallingAssembly()->Location;
+    const int c_PeHeaderOffset = 60;
+    const int c_LinkerTimestampOffset = 8;
+	array<uint8_t>^ b = gcnew array<uint8_t>(2048);
+	Stream^ s = nullptr;
+
+    try{
+		s = gcnew FileStream(filePath, System::IO::FileMode::Open, System::IO::FileAccess::Read);
+		s->Read(b, 0, b->Length);
+    }
+    finally{
+        if(s)
+			s->Close();
+    }
+
+    int i = System::BitConverter::ToInt32(b, c_PeHeaderOffset);
+    int secondsSince1970 = System::BitConverter::ToInt32(b, i + c_LinkerTimestampOffset);
+    DateTime dt = DateTime(1970, 1, 1, 0, 0, 0);
+    dt = dt.AddSeconds(secondsSince1970);
+	dt = dt.AddHours(TimeZone::CurrentTimeZone->GetUtcOffset(dt).Hours);
+    return dt;
+}
+
 [STAThreadAttribute]
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd){
 	Application::EnableVisualStyles();
@@ -18,7 +44,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		file->WriteLine("# App Switcher Error Report");
 		
 		file->WriteLine("## Environment");
-		file->WriteLine("### Date");
+		file->WriteLine("### Build Date");
+		file->WriteLine(RetrieveLinkerTimestamp().ToString());
+		file->WriteLine("### Crash Date");
 		file->WriteLine(DateTime::Now.ToString());
 
 		file->WriteLine("## Exception");
